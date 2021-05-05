1ebe023a376e7f36036f1982a4b302e97c7ca256
Tyler J. Perry <iamtylerjordan@gmail.com>
2021-05-04 23:13:50 -0400

    [wip] adding theme files & staring build

--- wp-content/themes/cgf/footer.php
+++ wp-content/themes/cgf/footer.php
@@ -11,21 +11,11 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cgf' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cgf' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cgf' ), 'cgf', '<a href="http://underscores.me/">Tyler J. Perry</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer class="page-footer py-4 px-3">
+        <div class="page-footer__inner container">
+            &copy; 2021 | Site by Tyler Jordan Perry
+        </div>
+    </footer>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
---
+++ wp-content/themes/cgf/front-page.php
@@ -0,0 +1,95 @@
+<?php get_header()?> 
+    <div class="page-header py-4 px-3">
+        <div class="page-header__inner container d-md-flex">
+            <div class="page-header__col-left">
+                <a href="">Instagram</a>
+                <a href="">YouTube</a>
+            </div>
+            <div class="page-header__col-center"></div>
+            <div class="page-header__col-right">
+                <span>Full site coming soon!</span>
+            </div>
+        </div>
+    </div>
+    <div class="page-hero px-3">
+        <div class="page-hero__inner container">
+            <figure>
+                <picture>
+                    <img src=""/>
+                </picture>
+            </figure>
+        </div>
+    </div>
+    <div class="page-quote py-5 px-3">
+        <div class="page-quote__inner container">
+            <div class="quote-icon"></div>
+            <div class="quote-text">
+                <p>As we let our own Light shine, we consciously give other people permission to do the same. As we are liberated from our own fear, our presence automatically liberates others.</p>
+                <span class="quote-credit">-  Mariannne Williamson</span>
+            </div>
+        </div>
+    </div>
+    <div class="page-cta py-5 px-3">
+        <div class="page-cta__inner container d-md-flex">
+            <div class="page-cta__col--text col pl-0">
+                <h3>About me</h3>
+                <p>Music is life's way of speaking straight from the soul. Every step in the process to bring a score to life, the technique, language, dramatic intent and sound all converge to communicate the most elusive moments and emotions of our lived experience. I'm happy to be a part of that story and that sharing, and hope to share it as long as I have breath in my lungs. </p>
+                <a class="btn"><span>Contact me</span><span class="arrow"></span></a>
+            </div>
+            <div class="page-cta__col--img col">
+                <figure>
+                    <picture>
+                        <img src=""/>
+                    </picture>
+                </figure>
+            </div>
+        </div>
+    </div>
+    <div class="page-section py-5 px-3">
+        <div class="page-section__inner container">
+            <div class="page-section__title">
+                <h2>Videos</h2>
+                <a class="cta--text"><span class="arrow"></div></a>
+            </div>
+            <div class="page-section__content">
+                <div class="content-img" data-content="">
+                    <figure>
+                        <picture>
+                            <img src=""/>
+                        </picture>
+                    </figure>
+                </div>
+                <div class="content-img" data-content="">
+                    <figure>
+                        <picture>
+                            <img src=""/>
+                        </picture>
+                    </figure>
+                </div>
+                <div class="content-img" data-content="">
+                    <figure>
+                        <picture>
+                            <img src=""/>
+                        </picture>
+                    </figure>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="page-section py-5 px-3">
+        <div class="page-section__inner container">
+            <div class="page-cta__col--text">
+                <h2>Let's connect</h2>
+                <p>this is some text</p>
+                <a class="btn"><span>Contact me</span> <span class="arrow"></span></a>
+            </div>
+            <div class="page-cta__col--img img--round">
+                <figure>
+                    <picture>
+                        <img src=""/>
+                    </picture>
+                </figure>
+            </div>
+        </div>
+    </div>
+<?php get_footer();?>
