5a11ac3a8ef3ecaa72ae7308388524fbdb3a11b4
Tyler J. Perry <iamtylerjordan@gmail.com>
2021-05-05 06:52:01 -0400

    final touches

--- wp-content/themes/cgf/front-page.php
+++ wp-content/themes/cgf/front-page.php
@@ -2,8 +2,8 @@
     <div class="page-header py-4 px-3">
         <div class="page-header__inner container d-md-flex align-items-center justify-content-between">
             <div class="page-header__col-left d-none d-md-flex">
-                <a class="mr-3" href="">Instagram</a>
-                <a href="">YouTube</a>
+                <a href="https://www.instagram.com/chantal.gabrielle.freeman/" target="_blank" class="mr-3" href="">Instagram</a>
+                <a href="https://www.youtube.com/channel/UCCjE9XP8xI8OVPfhSjn9OvQ" target="_blank">YouTube</a>
             </div>
             <div class="page-header__col-center">
                 <div class="logo m-auto m-md-0"></div>
@@ -14,17 +14,17 @@
         </div>
     </div>
     <div class="page-hero px-3">
-        <div class="page-hero__inner container">
+        <div class="page-hero__inner container img--cover">
             <figure>
                 <picture>
-                    <img src=""/>
+                    <img src="<?php echo get_bloginfo('template_url');?>/img/img00.jpg"/>
                 </picture>
             </figure>
         </div>
         <div class="page-hero__subtext container py-3 d-block d-md-none align-items-center justify-content-between">
             <div class="subtext__col text-center">
-                <a class="d-block mb-2" href="">Instagram</a>
-                <a class="d-block mb-2" href="">YouTube</a>
+                <a class="d-block text-center" href="https://www.instagram.com/chantal.gabrielle.freeman/" target="_blank">Instagram</a>
+                <a class="d-block text-center" href="https://www.youtube.com/channel/UCCjE9XP8xI8OVPfhSjn9OvQ" target="_blank">YouTube</a>
                 <span class="d-block">Full site coming soon!</span>
             </div>
         </div>
@@ -40,10 +40,10 @@
     </div>
     <div class="page-cta py-5 px-3">
         <div class="page-cta__inner container d-md-flex align-items-md-center flex-row-reverse">
-            <div class="page-cta__col--img col-md pr-md-0 pl-0 pl-md-3 mb-5">
+            <div class="page-cta__col--img img--cover col-md pr-md-0 pl-0 pl-md-3 mb-5">
                 <figure>
                     <picture>
-                        <img src=""/>
+                        <img src="<?php echo get_bloginfo('template_url');?>/img/img01.jpg"/>
                     </picture>
                 </figure>
             </div>
@@ -51,7 +51,7 @@
                 <div class="col__inner">
                     <h3 class="d-block mb-0">In My Own Words</h3>
                     <p class="d-block py-4">Music is life's way of speaking straight from the soul. Every step in the process to bring a score to life, the technique, language, dramatic intent and sound all converge to communicate the most elusive moments and emotions of our lived experience. I'm happy to be a part of that story and that sharing, and hope to share it as long as I have breath in my lungs.</p>
-                    <a class="btn"><span>View my resume</span><span class="arrow"></span></a>
+                    <a href="<?php echo get_bloginfo('template_url');?>/resume.pdf" target="_blank" class="btn"><span>View my resume</span><span class="arrow"></span></a>
                 </div>
             </div>
         </div>
@@ -60,38 +60,38 @@
         <div class="page-section__inner container">
             <div class="page-section__title d-md-flex align-items-center justify-content-between">
                 <h2>Videos</h2>
-                <a class="cta--text">Check out my YouTube channel<span class="arrow"></div></a>
+                <a href="https://www.youtube.com/channel/UCCjE9XP8xI8OVPfhSjn9OvQ" target="_blank" class="cta--text">Check out my YouTube channel<span class="arrow"></span></a>
             </div>
             <div class="page-section__content container px-0 py-5 d-md-flex">
-                <div class="content-item col-md" data-content="">
-                    <div class="content-img mb-3">
+                <div class="content-item col-md px-0" data-content="Qyxwxn4SuEk" data-title="Robert, toi que j'aime">
+                    <div class="content-img img--cover mb-3">
                         <figure>
                             <picture>
-                                <img src=""/>
+                                <img src="<?php echo get_bloginfo('template_url');?>/img/img03.jpg"/>
                             </picture>
                         </figure>
                     </div>
-                    <span class="content-item__title">Title</span>
+                    <span class="content-item__title">Robert, toi que j'aime</span>
                 </div>
-                <div class="content-item col-md" data-content="">
-                    <div class="content-img mb-3">
+                <div class="content-item col-md px-0 px-md-3" data-content="ySR-v4urg6Q" data-title="Come in quest'ora bruna">
+                    <div class="content-img img--cover mb-3">
                         <figure>
                             <picture>
-                                <img src=""/>
+                                <img src="<?php echo get_bloginfo('template_url');?>/img/img04.jpg"/>
                             </picture>
                         </figure>
                     </div>
-                    <span class="content-item__title">Title</span>
+                    <span class="content-item__title">Come in quest'ora bruna</span>
                 </div>
-                <div class="content-item col-md" data-content="">
-                    <div class="content-img mb-3">
+                <div class="content-item col-md px-0" data-content="bkKyBwg5818" data-title="Alabama Song">
+                    <div class="content-img img--cover mb-3">
                         <figure>
                             <picture>
-                                <img src=""/>
+                                <img src="<?php echo get_bloginfo('template_url');?>/img/img05.jpg"/>
                             </picture>
                         </figure>
                     </div>
-                    <span class="content-item__title">Title</span>
+                    <span class="content-item__title">Alabama Song</span>
                 </div>
             </div>
         </div>
@@ -102,18 +102,25 @@
                 <div class="col__inner">
                     <h2 class="d-block mb-0">Let's connect</h2>
                     <p class="d-block mb-0 py-4">Feel free to reach out for gigs, or if you wanna say hi!</p>
-                    <a class="btn"><span>Contact me</span> <span class="arrow"></span></a>
+                    <a href="mailto:cfreeman04@gmail.com" target="_blank" class="btn"><span>Contact me</span> <span class="arrow"></span></a>
                 </div>
             </div>
-            <div class="page-cta__col--img img--round col-md">
+            <div class="page-cta__col--img img--round img--cover col-md">
                 <div class="img__inner">
                     <figure>
                         <picture>
-                            <img src=""/>
+                            <img src="<?php echo get_bloginfo('template_url');?>/img/img02.jpg"/>
                         </picture>
                     </figure>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="modal-window">
+        <div class="close-btn">
+            <div class="close-btn__slice"></div>
+            <div class="close-btn__slice"></div>
+        </div>
+        <div class="video-container"></div>
     </div>
 <?php get_footer();?>
