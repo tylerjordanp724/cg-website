1e734097183abd9d3aa34862ff8bef084e7ec2c4
Tyler J. Perry <iamtylerjordan@gmail.com>
2021-05-05 03:34:13 -0400

    [wip] - fixed font paths; blocking out images

--- wp-content/themes/cgf/front-page.php
+++ wp-content/themes/cgf/front-page.php
@@ -1,12 +1,14 @@
 <?php get_header()?> 
     <div class="page-header py-4 px-3">
-        <div class="page-header__inner container d-md-flex">
-            <div class="page-header__col-left">
-                <a href="">Instagram</a>
+        <div class="page-header__inner container d-md-flex align-items-center justify-content-between">
+            <div class="page-header__col-left d-none d-md-flex">
+                <a class="mr-3" href="">Instagram</a>
                 <a href="">YouTube</a>
             </div>
-            <div class="page-header__col-center"></div>
-            <div class="page-header__col-right">
+            <div class="page-header__col-center">
+                <div class="logo m-auto m-md-0"></div>
+            </div>
+            <div class="page-header__col-right d-none d-md-block">
                 <span>Full site coming soon!</span>
             </div>
         </div>
@@ -19,6 +21,13 @@
                 </picture>
             </figure>
         </div>
+        <div class="page-hero__subtext container py-3 d-block d-md-none align-items-center justify-content-between">
+            <div class="subtext__col text-center">
+                <a class="d-block mb-2" href="">Instagram</a>
+                <a class="d-block mb-2" href="">YouTube</a>
+                <span class="d-block">Full site coming soon!</span>
+            </div>
+        </div>
     </div>
     <div class="page-quote py-5 px-3">
         <div class="page-quote__inner container">
@@ -30,65 +39,80 @@
         </div>
     </div>
     <div class="page-cta py-5 px-3">
-        <div class="page-cta__inner container d-md-flex">
-            <div class="page-cta__col--text col pl-0">
-                <h3>About me</h3>
-                <p>Music is life's way of speaking straight from the soul. Every step in the process to bring a score to life, the technique, language, dramatic intent and sound all converge to communicate the most elusive moments and emotions of our lived experience. I'm happy to be a part of that story and that sharing, and hope to share it as long as I have breath in my lungs. </p>
-                <a class="btn"><span>Contact me</span><span class="arrow"></span></a>
-            </div>
-            <div class="page-cta__col--img col">
+        <div class="page-cta__inner container d-md-flex align-items-md-center flex-row-reverse">
+            <div class="page-cta__col--img col-md pr-md-0 pl-0 pl-md-3 mb-5">
                 <figure>
                     <picture>
                         <img src=""/>
                     </picture>
                 </figure>
             </div>
+            <div class="page-cta__col--text col-md pl-0 ">
+                <div class="col__inner">
+                    <h3 class="d-block mb-0">In My Own Words</h3>
+                    <p class="d-block py-4">Music is life's way of speaking straight from the soul. Every step in the process to bring a score to life, the technique, language, dramatic intent and sound all converge to communicate the most elusive moments and emotions of our lived experience. I'm happy to be a part of that story and that sharing, and hope to share it as long as I have breath in my lungs.</p>
+                    <a class="btn"><span>View my resume</span><span class="arrow"></span></a>
+                </div>
+            </div>
         </div>
     </div>
     <div class="page-section py-5 px-3">
         <div class="page-section__inner container">
-            <div class="page-section__title">
+            <div class="page-section__title d-md-flex align-items-center justify-content-between">
                 <h2>Videos</h2>
-                <a class="cta--text"><span class="arrow"></div></a>
+                <a class="cta--text">Check out my YouTube channel<span class="arrow"></div></a>
             </div>
-            <div class="page-section__content">
-                <div class="content-img" data-content="">
-                    <figure>
-                        <picture>
-                            <img src=""/>
-                        </picture>
-                    </figure>
+            <div class="page-section__content container px-0 py-5 d-md-flex">
+                <div class="content-item col-md" data-content="">
+                    <div class="content-img mb-3">
+                        <figure>
+                            <picture>
+                                <img src=""/>
+                            </picture>
+                        </figure>
+                    </div>
+                    <span class="content-item__title">Title</span>
                 </div>
-                <div class="content-img" data-content="">
-                    <figure>
-                        <picture>
-                            <img src=""/>
-                        </picture>
-                    </figure>
+                <div class="content-item col-md" data-content="">
+                    <div class="content-img mb-3">
+                        <figure>
+                            <picture>
+                                <img src=""/>
+                            </picture>
+                        </figure>
+                    </div>
+                    <span class="content-item__title">Title</span>
                 </div>
-                <div class="content-img" data-content="">
-                    <figure>
-                        <picture>
-                            <img src=""/>
-                        </picture>
-                    </figure>
+                <div class="content-item col-md" data-content="">
+                    <div class="content-img mb-3">
+                        <figure>
+                            <picture>
+                                <img src=""/>
+                            </picture>
+                        </figure>
+                    </div>
+                    <span class="content-item__title">Title</span>
                 </div>
             </div>
         </div>
     </div>
     <div class="page-section py-5 px-3">
-        <div class="page-section__inner container">
-            <div class="page-cta__col--text">
-                <h2>Let's connect</h2>
-                <p>this is some text</p>
-                <a class="btn"><span>Contact me</span> <span class="arrow"></span></a>
+        <div class="page-section__inner container d-md-flex align-items-md-center">
+            <div class="page-cta__col--text col-md mb-5 mb-md-0">
+                <div class="col__inner">
+                    <h2 class="d-block mb-0">Let's connect</h2>
+                    <p class="d-block mb-0 py-4">Feel free to reach out for gigs, or if you wanna say hi!</p>
+                    <a class="btn"><span>Contact me</span> <span class="arrow"></span></a>
+                </div>
             </div>
-            <div class="page-cta__col--img img--round">
-                <figure>
-                    <picture>
-                        <img src=""/>
-                    </picture>
-                </figure>
+            <div class="page-cta__col--img img--round col-md">
+                <div class="img__inner">
+                    <figure>
+                        <picture>
+                            <img src=""/>
+                        </picture>
+                    </figure>
+                </div>
             </div>
         </div>
     </div>
